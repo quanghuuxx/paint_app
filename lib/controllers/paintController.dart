1c98e3d99b869e8f4ad67f9e303d9c2ad6a0ef0f
conbo104@gmail.com <conbo104@gmail.com>
2021-03-29 15:08:38 +0700

    format code

---
+++ lib/controllers/paintController.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+import 'package:get/get.dart';
+
+class ControllerPaintPage extends GetxController {}
+
+class Controller {
+  List<Path> paths = new List<Path>();
+  List<Paint> paintss = new List<Paint>();
+}
